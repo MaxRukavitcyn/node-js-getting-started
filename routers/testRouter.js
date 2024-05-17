64db300057182a228bb5b96624d6b22e9780716e
Max <rukav77@gmail.com>
2024-05-17 22:12:04 +0500

    new commit

--- routers/testRouter.js
+++ routers/testRouter.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require("path");
 const testRouter = express.Router()
 
 
 testRouter.get('/hello_world', function(req, res) {
 	res.send('hello world');
+});
+
+testRouter.get('/textFileTest',(request, response) => {
+	response.sendFile(path.resolve('public/textFileTest.html'))
+});
+
+testRouter.get('/forEkaterina',(request, response) => {
+	response.sendFile(path.resolve('public/forEkaterina.html'))
 });
 
 module.exports = testRouter;
